1e81ff59f3e371355064c897c53b4f020af949fe
prabakar.singaram <prabakar.singaram@PrabakarS.newwave.com>
2013-11-18 00:52:32 -0500

    test one

--- webproject/src/main/java/com/nwt/webproject/Test.java
+++ webproject/src/main/java/com/nwt/webproject/Test.java
@@ -6,7 +6,6 @@ private static String MESSAGE_NEW = "SOME TESTING";
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("hello!!!    "+MESSAGE_NEW);
 	}
 }
