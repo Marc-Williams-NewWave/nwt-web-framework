3673810dc21eaf0d6628b2bef17172099c7d2cdd
prabakar.singaram <prabakar.singaram@PrabakarS.newwave.com>
2013-11-17 23:12:25 -0500

    some more test

--- webproject/src/main/java/com/nwt/webproject/Test.java
+++ webproject/src/main/java/com/nwt/webproject/Test.java
@@ -1,14 +1,13 @@
 package com.nwt.webproject;
 
 public class Test {
-	private static final String MESSAGE = "hello dear!!!";
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello!!!   "+MESSAGE);
+		System.out.println("hello!!!   ");
 
 	}
 
