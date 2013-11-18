306dae9ea43312657e79b200df8fe2fc601bb984
prabakar.singaram <prabakar.singaram@PrabakarS.newwave.com>
2013-11-17 23:26:51 -0500

    FINAL TEST

--- webproject/src/main/java/com/nwt/webproject/Test.java
+++ webproject/src/main/java/com/nwt/webproject/Test.java
@@ -1,14 +1,12 @@
 package com.nwt.webproject;
 
 public class Test {
-
+private static String MESSAGE_NEW = "SOME TESTING"; 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello!!!");
-
+		System.out.println("hello!!!    "+MESSAGE_NEW);
 	}
-
 }
